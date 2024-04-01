b2daab84cb8ca2e5d96520b3a88ad7bb39c818a1
Vladyslav Vagin <140642012+VladyslavVagin@users.noreply.github.com>
2024-04-02 00:04:34 +0200

    add database firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,9 +1,11 @@
 import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCJ6xbt9tROzc55OgY4uZayVgcc7d2JmBE",
   authDomain: "learnlingo-32d7b.firebaseapp.com",
+  databaseURL: "https://learnlingo-32d7b-default-rtdb.europe-west1.firebasedatabase.app/",
   projectId: "learnlingo-32d7b",
   storageBucket: "learnlingo-32d7b.appspot.com",
   messagingSenderId: "723673104408",
@@ -12,6 +14,7 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
 const auth = getAuth(app);
 
-export { app, auth };
+export { app, auth, database };
