2698dedd0836b166fda2eabf1c3a67e7a2af5432
Vladyslav Vagin <140642012+VladyslavVagin@users.noreply.github.com>
2024-04-14 10:22:14 +0200

    clear comments

--- src/components/FavoritesPage/ListFavorites/ListFavorites.jsx
+++ src/components/FavoritesPage/ListFavorites/ListFavorites.jsx
@@ -10,37 +10,19 @@ import { getFavorites } from "../../../firebase/api";
 const ListFavorites = () => {
   const [favorites, setFavorites] = useState(null)
 
-  // useEffect(() => {
-  //   const getData = async () => {
-  //     try {
-  //     const datas = await getFavorites();
-  //     console.log(datas);
-  //     setFavorites(datas);
-  //     } catch {
-  //       toast.error('Database error');
-  //     }
-  //   }
-  //   getData();
-  //   return () => getData();
-  // }, [])
   useEffect(() => {
-    // Listen for authentication state changes
     const unsubscribe = onAuthStateChanged(auth, async (user) => {
       if (user) {
         try {
-          // Fetch favorites when user is logged in
           const favData = await getFavorites();
           setFavorites(favData);
         } catch (error) {
           toast.error("Error fetching favorites");
         }
       } else {
-        // Clear favorites when user logs out
         setFavorites(null);
       }
     });
-
-    // Unsubscribe from the listener when component unmounts
     return () => unsubscribe();
   }, []);
 
